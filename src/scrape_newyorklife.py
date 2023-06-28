9b02f6018af7666800eb9dc4f79ba631c1292816
Marcos Keyser <marcosak@gmail.com>
2023-06-28 12:44:12 -0400

    Add scrape_newyorklife.py to use WebScraper class for New York Life data acquisition

---
+++ src/scrape_newyorklife.py
@@ -0,0 +1,10 @@
+from web_scraper import WebScraper
+
+if __name__ == "__main__":
+    scraper = WebScraper()
+
+    # Fetch the URLs from the sitemap
+    urls = scraper.get_urls_in_sitemap("https://www.newyorklife.com/sitemap.xml")
+
+    # Scrape each URL and save the contents to a text file
+    scraper.save_to_files(urls)
